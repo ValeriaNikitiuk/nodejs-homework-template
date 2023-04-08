96353b0ecdfffa84d29b51bcc56a1da2b7ddfda0
ValeriaOvod <108237625+ValeriaNikitiuk@users.noreply.github.com>
2023-04-08 20:58:32 +0300

    /

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 const app = require('./app');
-// 8NtEeLUEDfATyaOE
-const DB_HOST = "mongodb+srv://Valeria:8NtEeLUEDfATyaOE@cluster0.nafxeoi.mongodb.net/db-contacts?retryWrites=true&w=majority";
-
+const { DB_HOST } = require('./config');
 mongoose.connect(DB_HOST)
   .then(() => app.listen(3000))
   .catch(error => console.log(error.message));
