ef1267c247374ddbabb5ddb3123b4139f40e990c
ValeriaOvod <108237625+ValeriaNikitiuk@users.noreply.github.com>
2023-04-08 20:32:17 +0300

    /

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-const app = require('./app')
+const mongoose = require('mongoose');
+const app = require('./app');
+// 8NtEeLUEDfATyaOE
+const DB_HOST = "mongodb+srv://Valeria:8NtEeLUEDfATyaOE@cluster0.nafxeoi.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose.connect(DB_HOST)
+  .then(() => app.listen(3000))
+  .catch(error => console.log(error.message));
+
